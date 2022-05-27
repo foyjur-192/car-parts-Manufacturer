8277543dc053edc740d969946291ade4410e4352
foyjur-192 <97134368+foyjur-192@users.noreply.github.com>
2022-05-27 20:05:34 +0600

    updatebtn

--- src/Page/Home/Contact.js
+++ src/Page/Home/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
               <label for="email" class="leading-7 text-left text-sm text-gray-600">Email</label>
               <input type="email" id="email" name="email" placeholder='Email' class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
             </div>
-            <button class="text-white bg-secondary border-0 py-2 px-8 focus:outline-none hover:bg-white hover:text-secondary rounded text-lg">Button</button>
+            <button class="text-white bg-secondary border-0 btn  mt-5 focus:outline-none hover:bg-white hover:text-secondary rounded text-lg">Submit</button>
           </div>
         </div>
       </section>
